5b155eaadf416b4ff63a30cfa0bb1122e207eaca
Guillaume Dueymes <33063781+guimeto@users.noreply.github.com>
2022-12-13 10:25:10 +0100

    Update urls.py

--- apps/instrumentation/urls.py
+++ apps/instrumentation/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('Gault', views.SentinelGaultView.as_view(), name='sentinelGault_instrumentation'),
     path('sentinel2', views.Sentinel2View.as_view(), name='sentinel2_instrumentation'),
     path('sentinel3', views.Sentinel3View.as_view(), name='sentinel3_instrumentation'),
+    path('Scanning_Doppler', views.ScanningView.as_view(), name='scanning_doppler_instrumentation'),
 
 
     ]
