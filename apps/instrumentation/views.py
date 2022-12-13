a0c9775d8a33091ba8d675eb0958fe2df969e8a3
Guillaume Dueymes <33063781+guimeto@users.noreply.github.com>
2022-12-13 11:53:26 +0100

    Update views.py

--- apps/instrumentation/views.py
+++ apps/instrumentation/views.py
@@ -55,5 +55,15 @@ class ScanningView(TemplateView):
         context['title'] = 'Sentinel 3 titre a garder comme tu veux'
         context['stations'] = Station.objects.all().values('id', 'name')
         return context
+    
+    
+ class AERIView(TemplateView):
+    template_name = "instrumentation/aeri.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Sentinel 3 titre a garder comme tu veux'
+        context['stations'] = Station.objects.all().values('id', 'name')
+        return context
 
 
