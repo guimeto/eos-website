cb06bf5af962536687528849f4691a0c4778cada
Guillaume Dueymes <33063781+guimeto@users.noreply.github.com>
2022-12-15 14:05:04 +0100

    Update urls.py

--- apps/instrumentation/urls.py
+++ apps/instrumentation/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('biology', views.BiologyView.as_view(), name='biology'),
     path('Phenotyping', views.PhenotypingView.as_view(), name='Phenotyping'),
     path('mobile_climate', views.mobileView.as_view(), name='mobile_climate'),
+    path('MetBoxes', views.MetBoxesView.as_view(), name='MetBoxes'),
 
     ]
